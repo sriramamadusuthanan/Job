f3bf20ea7f33726e84b6cc37f0b6aa61c5fb39c9
sriramamadusuthanan <srirama1999sri@gmail.com>
2024-12-28 11:41:42 +0530

    first commit from sriram ,the basic spring project

---
+++ src/main/java/com/project/JobPortal/JobPortalApplication.java
@@ -0,0 +1,15 @@
+package com.project.JobPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JobPortalApplication.class, args);
+		System.out.println("API Started");
+	}
+
+}
